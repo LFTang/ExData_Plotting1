58a2a8f71eed3ad99c552305577ae6703ebedbe3
LFTang <fb1335@wayne.edu>
2015-11-09 03:12:59 -0500

    plot3.*

--- plot3.R
+++ plot3.R
@@ -11,8 +11,7 @@ png(filename="plot3.png", width=480, height=480, units="px")
 plot(dt.active$datetime, dt.active$Sub_metering_1, type = 'l', xlab = '', ylab = 'Energy sub metering')
 lines(dt.active$datetime, dt.active$Sub_metering_2, col='red')
 lines(dt.active$datetime, dt.active$Sub_metering_3, col='blue')
-
-legend('topright', lwd = 1, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), cex = 0.5) 
+legend('topright', lwd = 1, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')) 
 
 dev.off()
 
